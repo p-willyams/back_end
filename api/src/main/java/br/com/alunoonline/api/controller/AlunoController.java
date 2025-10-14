7364517566d3092d2a4ec5ccffc81050f41efd1c
Patryck Willyams <patryckwilliamys@gmail.com>
2025-10-14 10:27:22 -0300

    first commit

---
+++ api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -0,0 +1,36 @@
+package br.com.alunoonline.api.controller;
+
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    @Autowired
+    AlunoService alunoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarAluno(@RequestBody Aluno aluno){
+        alunoService.criarAluno(aluno);
+    }
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> buscarTodosAlunos(){
+        return alunoService.buscarTodosAlunos();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> buscarAlunoPeloID(@PathVariable Long id){
+        return alunoService.buscarAlunoPeloID(id);
+    }
+
+}
---
+++ api/src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -0,0 +1,29 @@
+package br.com.alunoonline.api.service;
+
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    AlunoRepository alunoRepository;
+
+    public void criarAluno(Aluno aluno){
+        alunoRepository.save(aluno);
+
+    }
+    public List<Aluno> buscarTodosAlunos(){
+        return alunoRepository.findAll();
+
+    }
+    public Optional<Aluno> buscarAlunoPeloID(Long id){
+        return alunoRepository.findById(id);
+
+    }
+
+}
